60b798c8190e8569891a6cbd693cf82f198c3826
quinton jones <kjdatruf@hotmail.com>
2020-06-07 11:27:17 -0400

    ch 12 updates

--- app/src/main/java/com/bignerdranch/android/criminallintent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminallintent/MainActivity.kt
@@ -2,8 +2,12 @@ package com.bignerdranch.android.criminallintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity(),
+    CrimeListFragment.Callbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,11 +16,20 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragment_container)
 
         if (currentFragment == null) {
-            val fragment = CrimeListFragment.newInstance()
+            val fragment = CrimeFragment()
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment_container, fragment)
                 .commit()
         }
     }
+        override fun onCrimeSelected(crimeId: UUID) {
+            val fragment = CrimeFragment.newInstance(crimeId)
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
 }
